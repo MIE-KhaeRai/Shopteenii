27605143f1c9f22ae6eabd2771caf93079817486
KatsuTangent <52738973+KatsuTangent@users.noreply.github.com>
2020-06-29 14:37:14 +0700

    finish

---
+++ app/Http/Controllers/ProductSController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\product_s;
+use Illuminate\Http\Request;
+
+class ProductSController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\product_s  $product_s
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product_s $product_s)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\product_s  $product_s
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(product_s $product_s)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\product_s  $product_s
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, product_s $product_s)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\product_s  $product_s
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product_s $product_s)
+    {
+        //
+    }
+}
